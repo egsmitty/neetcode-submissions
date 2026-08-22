2eafbd478c7fc6b7803a732a72b1d2973d3586ca
Ethan Smith <ethansmithg@gmail.com>
2026-08-22 16:53:57 -0500

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int missing = 0;
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= i;
+            missing ^= nums[i];
+        }
+        missing ^= nums.length;
+        return missing;
+    }
+}
