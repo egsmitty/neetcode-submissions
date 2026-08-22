ff153d1a42359bb6785c2a5d8b86eade01385954
Ethan Smith <ethansmithg@gmail.com>
2026-08-22 10:47:54 -0500

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            if (numSet.contains(num)) {
+                return num;
+            }
+            numSet.add(num);
+        }
+        return -1;
+    }
+}
